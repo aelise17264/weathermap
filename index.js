4feb4d39b409db0a0217224a96f9508744a593a4
unknown <aelise17264@gmail.com>
2021-03-25 21:34:49 -0600

    set up weather app to by dynamic based on user input

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express")
 
 const https = require('https');
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=17c6fe1cd23655b9668e47afe891c9da"
+    res.sendFile(__dirname + '/index.html')
+
+})
+
+app.post("/", function(req, res){
+    const city = req.body.cityName
+    const apiKey = "17c6fe1cd23655b9668e47afe891c9da"
+    const units = "imperial"
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&units=" + units + "&appid=" + apiKey;
     https.get(url, function(response){
         console.log(response.statusCode)
         response.on("data", function(data){
@@ -15,10 +26,10 @@ app.get('/', function(req, res){
             const icon = weatherData.weather[0].icon
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<p>The weather is currently " + description + "</p>")
-           res.write("<h1>The current temperature in Denver is " + temp + " degrees F</h1>")
+           res.write("<h1>The current temperature in " + city + " is " + temp + " degrees F</h1>")
            res.write("<img src=" + imageURL + ">")
            res.send()
-        })
+       })
     })
 
 })
