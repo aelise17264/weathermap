f2a2e561cd7d6a6b1054713fd6492f8c013992b0
unknown <aelise17264@gmail.com>
2021-05-02 18:47:31 -0600

    added dynamic port

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ app.post("/", function(req, res){
 
 })
 
-app.listen(3000, function(){
-    console.log("Server running on port 3000")
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log("Server running on port " + port)
 })
